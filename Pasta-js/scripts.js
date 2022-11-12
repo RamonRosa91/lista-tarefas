641de519d3134edf9dd0fb3ff82ca9dcd475901e
Ramon Rosa <ramon.costa.rosa@gmail.com>
2022-11-11 21:10:44 -0300

    Adicionado Resultado de Tarefa
    JS: Adicionado div de tarefas feitas
    CSS: Personalizado botão de delete
    HTML: Adicionado script de icon

--- Pasta-js/scripts.js
+++ Pasta-js/scripts.js
@@ -1,5 +1,6 @@
 const inputElement = document.querySelector(".tarefas-input");
 const addTaskButton = document.querySelector(".adicionar-tarefa-botao");
+const tasksContainer = document.querySelector(".resultado-tarefas");
 
 const validateInput = () => inputElement.value.trim().length > 0;
 
@@ -9,6 +10,23 @@ const handleAddTask = () => {
     if(!inputIsValid) {
        return inputElement.classList.add("erro");
     }
+
+const taskItemContainer = document.createElement('div');
+taskItemContainer.classList.add("task-item");
+
+const taskContent = document.createElement('p');
+
+taskContent.innerText = inputElement.value;
+
+const deleteItem = document.createElement("i");
+deleteItem.classList.add('fa-solid');
+deleteItem.classList.add('fa-trash-can');
+
+taskItemContainer.appendChild(taskContent);
+taskItemContainer.appendChild(deleteItem);
+tasksContainer.appendChild(taskItemContainer);
+
+inputElement.value = "";
 };
 
 const handleInputChange = () => {
