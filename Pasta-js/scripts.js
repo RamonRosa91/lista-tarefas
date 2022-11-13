050dd40b65d4ec86c424a968c54d1cb19fcc4545
Ramon Rosa <ramon.costa.rosa@gmail.com>
2022-11-12 23:03:29 -0300

    Adicionado funções aos botões
    Adicionado o remove item no botão de "trash" e adicionado CSS no botão de "feito"

--- Pasta-js/scripts.js
+++ Pasta-js/scripts.js
@@ -18,16 +18,45 @@ const taskContent = document.createElement('p');
 
 taskContent.innerText = inputElement.value;
 
+const doneTask = document.createElement("i");
+doneTask.classList.add('fa-solid');
+doneTask.classList.add('fa-check-to-slot');
+
+doneTask.addEventListener("click", () => handleClick(taskContent));
+
 const deleteItem = document.createElement("i");
 deleteItem.classList.add('fa-solid');
 deleteItem.classList.add('fa-trash-can');
 
+deleteItem.addEventListener("click", () => handleDeleteClick (taskItemContainer, taskContent));
+
 taskItemContainer.appendChild(taskContent);
+taskItemContainer.appendChild(doneTask);
 taskItemContainer.appendChild(deleteItem);
 tasksContainer.appendChild(taskItemContainer);
 
 inputElement.value = "";
 };
+
+const handleClick = (taskContent) => {
+    const tasks = tasksContainer.childNodes;
+    
+    for (const task of tasks) {
+        if (task.firstChild.isSameNode(taskContent)) {
+            task.firstChild.classList.toggle("completed");
+        }
+    }
+}
+
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+    const tasks = tasksContainer.childNodes;
+
+    for (const task of tasks) {
+        if (task.firstChild.isSameNode(taskContent)) {
+            taskItemContainer.remove();
+        }
+    }
+}
 
 const handleInputChange = () => {
     const inputIsValid = validateInput ();
